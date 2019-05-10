12efd00c7769f2056591b05e252d8f91f7ed33c3
Allison Howard <alliemhow@gmail.com>
2019-05-10 08:37:07 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,13 +1,13 @@
-
 require_relative '../../config/environment'
 
 class ApplicationController < Sinatra::Base
-
   configure do
     set :public_folder, 'public'
     set :views, 'app/views'
   end
 
   get '/' do
+    redirect to "/articles"
   end
+
 end
